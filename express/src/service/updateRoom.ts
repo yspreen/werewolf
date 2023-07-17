845f59bf5dde13e7e171def704a82b66c356ee80
Yannick <yannick.spreen@gmail.com>
2023-07-17 16:07:52 -0400

    Add reasonable starting numbers for roles.

--- express/src/service/updateRoom.ts
+++ express/src/service/updateRoom.ts
@@ -19,6 +19,7 @@ export function setWinner(room: Room, winner: WINNER) {
 }
 
 export async function checkWinner(room: Room, includeDeathsFromTonight: boolean) {
+  if (!room.givenRoles) return
   if (room.winner) return setWinner(room, room.winner)
 
   const alive = room.memberIds.filter((userId) => {
