3d8b4bafb765380c9f1420cfd7ded40284f85496
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-07-17 17:15:59 -0400

    Fix build script.

--- express/build.sh
+++ express/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-tsc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+nvm use
+
+yarn tsc
 yarn install --prod
 rm -rf bundle
 mkdir bundle
