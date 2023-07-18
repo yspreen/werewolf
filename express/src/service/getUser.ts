d893de9aab2e00ec440d660f36ce834156d831b8
Yannick <yannick.spreen@gmail.com>
2023-07-17 23:01:11 -0400

    Fix cookie same site.

--- express/src/service/getUser.ts
+++ express/src/service/getUser.ts
@@ -1,38 +1,37 @@
-import { Request, Response } from "express";
-import { uuid } from "./uuid";
-import { redis } from "./redis";
-import { USER_TTL_SECONDS } from "./config";
-import { updateUser } from "./updateUser";
-import { User, newUser } from "../models/user";
+import { Request, Response } from 'express'
+import { uuid } from './uuid'
+import { redis } from './redis'
+import { USER_TTL_SECONDS } from './config'
+import { updateUser } from './updateUser'
+import { User, newUser } from '../models/user'
 
 export async function getUserId(req: Request, res: Response): Promise<string> {
-  let sessionId = req.cookies.sessionId || "";
-  let userId = await redis.get(`session:${sessionId}`);
+  let sessionId = req.cookies.sessionId || ''
+  let userId = await redis.get(`session:${sessionId}`)
 
   if (!userId) {
-    sessionId = uuid();
-    userId = uuid();
-    res.cookie("sessionId", sessionId, {
+    sessionId = uuid()
+    userId = uuid()
+    res.cookie('sessionId', sessionId, {
       maxAge: USER_TTL_SECONDS * 1000,
-    });
-    await redis.set(`session:${sessionId}`, userId, { EX: USER_TTL_SECONDS });
-    await updateUser(newUser(userId));
+      sameSite: 'none',
+      secure: true
+    })
+    await redis.set(`session:${sessionId}`, userId, { EX: USER_TTL_SECONDS })
+    await updateUser(newUser(userId))
   }
-  return userId;
+  return userId
 }
 
 export async function getUser(req: Request, res: Response): Promise<User> {
-  const userId = await getUserId(req, res);
-  const userString = await redis.get(`user:${userId}:profile`);
-  if (userString) return JSON.parse(userString);
-  const user = newUser(userId);
-  updateUser(user);
-  return user;
+  const userId = await getUserId(req, res)
+  const userString = await redis.get(`user:${userId}:profile`)
+  if (userString) return JSON.parse(userString)
+  const user = newUser(userId)
+  updateUser(user)
+  return user
 }
 
 export async function findUser(userId: string): Promise<User> {
-  return JSON.parse(
-    (await redis.get(`user:${userId}:profile`)) ??
-      JSON.stringify(newUser(userId))
-  );
+  return JSON.parse((await redis.get(`user:${userId}:profile`)) ?? JSON.stringify(newUser(userId)))
 }
