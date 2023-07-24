622f1e65f66e8083be3c27ad898d4c26b2962971
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-07-24 18:39:36 -0400

    Catch missing room error correctly & return to room list.

--- express/src/endpoints/getRoom.ts
+++ express/src/endpoints/getRoom.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express";
-import { getRoom } from "../service/getRoom";
-import { getCleanRoomIds } from "../service/getCleanRoomIds";
+import { Request, Response } from 'express'
+import { getRoom } from '../service/getRoom'
+import { getCleanRoomIds } from '../service/getCleanRoomIds'
 
 export async function getRoomEndpoint(req: Request, res: Response) {
-  const ids = await getCleanRoomIds();
-  const { roomId } = req.params;
+  const ids = await getCleanRoomIds()
+  const { roomId } = req.params
   if (!ids.includes(roomId)) {
-    return res.json({});
+    return res.json({ room: { memberIds: [] } })
   }
-  const room = await getRoom(roomId);
-  res.json({ room });
+  const room = await getRoom(roomId)
+  res.json({ room })
 }
